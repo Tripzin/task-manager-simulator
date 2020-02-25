26fbcd90bf177fa8b9dbd99bf09297b805b95681
Tripzin <kingyoikusei@gmail.com>
2020-02-25 20:22:34 +0900

    Updates

---
+++ precache-manifest.19651a53c9163135a36b1309a865beab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3fbdb11c9b39a83e93c7c7a12f815c",
+    "url": "/task-manager-simulator/index.html"
+  },
+  {
+    "revision": "036f58db49829271bad4",
+    "url": "/task-manager-simulator/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5a530478ac2f8af10e1d",
+    "url": "/task-manager-simulator/static/css/main.ee3673a3.chunk.css"
+  },
+  {
+    "revision": "036f58db49829271bad4",
+    "url": "/task-manager-simulator/static/js/2.2eea03e9.chunk.js"
+  },
+  {
+    "revision": "977b85ae92e0fb68aebdece70c5a2518",
+    "url": "/task-manager-simulator/static/js/2.2eea03e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a530478ac2f8af10e1d",
+    "url": "/task-manager-simulator/static/js/main.fc0d5c45.chunk.js"
+  },
+  {
+    "revision": "c4869c3d38d0fac0e77e",
+    "url": "/task-manager-simulator/static/js/runtime-main.12aa5210.js"
+  }
+]);
